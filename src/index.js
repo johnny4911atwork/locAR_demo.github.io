a12b8d33e556709a25b8482ba74db6753700921e
johnny4911atwork <johnny4911atwork@gmail.com>
2025-11-03 15:02:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as THREE from "https://esm.sh/three";
 import * as LocAR from 'https://esm.sh/locar';
 
 const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 1.6, 0);
+camera.position.set(0, 2.0, 0);
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.getElementById('glscene')
